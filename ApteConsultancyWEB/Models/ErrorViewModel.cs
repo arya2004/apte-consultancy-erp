740a0235bcc1ba78d1de8510417539bc6cf0a9ae
arya2004 <arya20j4@gmail.com>
2023-08-18 00:04:42 +0530

    Add project files.

---
+++ ApteConsultancyWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApteConsultancyWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
